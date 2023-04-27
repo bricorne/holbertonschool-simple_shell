3c30c1d30ba8364bf96649be5b7f81d6cf64d8ab
bricorne <bricepoitevineau@gmail.com>
2023-04-27 05:07:59 -0700

    /

--- execute.c
+++ execute.c
@@ -5,10 +5,15 @@ void execute_command(char **args)
 	int status;
 	char *path = "/bin/";
 	char *full_path = malloc(strlen(path) + strlen(args[0]) + 1);
-
-	strcpy(full_path, path);
-	strcat(full_path, args[0]);
-
+	if (args[0][0] == '/')
+	{
+		full_path = args[0];
+	}
+	else
+	{
+		strcpy(full_path, path);
+		strcat(full_path, args[0]);
+	}
 	pid = fork();
 
 	if (pid == -1)
